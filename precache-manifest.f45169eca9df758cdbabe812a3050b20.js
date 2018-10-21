814c89710563941b6a079023f32eed1b4519da00
Nishan Bajracharya <nisbaj11@gmail.com>
2018-10-21 17:23:53 +0545

    Updates

--- precache-manifest.f45169eca9df758cdbabe812a3050b20.js
+++ precache-manifest.f45169eca9df758cdbabe812a3050b20.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/balance-pwa/static/media/AvenirNextLTPro-Light.7be5411c.otf"
   },
   {
-    "revision": "ea86088c7eef9ecba132",
-    "url": "/balance-pwa/static/css/main.7fc07b68.chunk.css"
+    "revision": "18253a58fff2af95052e",
+    "url": "/balance-pwa/static/css/main.4dcdde1a.chunk.css"
   },
   {
     "revision": "f1e786c91408b15a9728",
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/balance-pwa/static/js/runtime~main.46036c7c.js"
   },
   {
-    "revision": "b62f8f0b14aebd847dcc75a459018f69",
-    "url": "/balance-pwa/static/media/AvenirNextLTPro-Regular.b62f8f0b.otf"
-  },
-  {
-    "revision": "ea86088c7eef9ecba132",
-    "url": "/balance-pwa/static/js/main.ea86088c.chunk.js"
-  },
-  {
     "revision": "751cdac5cfc9c37deefa6180a27a831d",
     "url": "/balance-pwa/static/media/AvenirNextLTPro-Medium.751cdac5.otf"
+  },
+  {
+    "revision": "18253a58fff2af95052e",
+    "url": "/balance-pwa/static/js/main.18253a58.chunk.js"
+  },
+  {
+    "revision": "b62f8f0b14aebd847dcc75a459018f69",
+    "url": "/balance-pwa/static/media/AvenirNextLTPro-Regular.b62f8f0b.otf"
   },
   {
     "revision": "472d97a823bc74eca3293c8198208d89",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/balance-pwa/static/media/sync.4a9165a2.svg"
   },
   {
-    "revision": "385ad6ca9fa1589a87a4f196f779a8a3",
+    "revision": "c5e1e4261f2b2a3dad60a4d13bc7d514",
     "url": "/balance-pwa/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/balance-pwa/precache-manifest.acdc8f8f466a563f42cf337acb1b32f9.js"
+  "/balance-pwa/precache-manifest.f45169eca9df758cdbabe812a3050b20.js"
 );
 
 workbox.clientsClaim();
