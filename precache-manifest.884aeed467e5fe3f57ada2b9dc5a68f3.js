53e7e88b121f530b293d572149e90094b4b95ace
Nishan Bajracharya <nisbaj11@gmail.com>
2018-10-21 16:52:33 +0545

    Updates

---
+++ precache-manifest.884aeed467e5fe3f57ada2b9dc5a68f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7be5411c85de426f348a8754f082d17e",
+    "url": "/balance-pwa/static/media/AvenirNextLTPro-Light.7be5411c.otf"
+  },
+  {
+    "revision": "5b35953039bf207edc63",
+    "url": "/balance-pwa/static/css/main.7fc07b68.chunk.css"
+  },
+  {
+    "revision": "f1e786c91408b15a9728",
+    "url": "/balance-pwa/static/js/1.f1e786c9.chunk.js"
+  },
+  {
+    "revision": "46036c7c35c27d3c4575",
+    "url": "/balance-pwa/static/js/runtime~main.46036c7c.js"
+  },
+  {
+    "revision": "b62f8f0b14aebd847dcc75a459018f69",
+    "url": "/balance-pwa/static/media/AvenirNextLTPro-Regular.b62f8f0b.otf"
+  },
+  {
+    "revision": "5b35953039bf207edc63",
+    "url": "/balance-pwa/static/js/main.5b359530.chunk.js"
+  },
+  {
+    "revision": "751cdac5cfc9c37deefa6180a27a831d",
+    "url": "/balance-pwa/static/media/AvenirNextLTPro-Medium.751cdac5.otf"
+  },
+  {
+    "revision": "472d97a823bc74eca3293c8198208d89",
+    "url": "/balance-pwa/static/media/AvenirNextLTPro-Demi.472d97a8.otf"
+  },
+  {
+    "revision": "bcd47f6df43d8c4432b3062b3d0e0e72",
+    "url": "/balance-pwa/static/media/back.bcd47f6d.svg"
+  },
+  {
+    "revision": "4a9165a2f49b8cdff374d6a3161d8df3",
+    "url": "/balance-pwa/static/media/sync.4a9165a2.svg"
+  },
+  {
+    "revision": "b86aed27c1e764c4872028642d104e97",
+    "url": "/balance-pwa/index.html"
+  }
+];
